c1723e77e3e5fcaf67f2122f6dc424794caefa09
Csmally <1130180692@qq.com>
2025-02-19 23:15:12 +0800

    第一次提交

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    { languageOptions: { globals: globals.browser } },
+    {
+        files: ["packages/**/*.{ts,tsx}"],
+        rules: {
+            "no-undef": "error",
+            "no-unused-vars": "error",
+            "no-console": "warn",
+        },
+    },
+];
